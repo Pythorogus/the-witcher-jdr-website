4d0dd1ab3d49f754779f66156ff3dd84cf57824b
Pythorogus <pierre.gomba@gmail.com>
2020-04-28 02:39:50 +0200

    index theme

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,17 @@ import 'bootstrap';
 import 'popper.js';
 
 import { library,dom } from '@fortawesome/fontawesome-svg-core';
-// import '@fortawesome/free-solid-svg-icons';
+import {
+    faDownload, faUser, faUsers, faSignInAlt, faSignOutAlt
+} from '@fortawesome/free-solid-svg-icons';
 // import '@fortawesome/free-regular-svg-icons'
 import {
-    faFacebookSquare, faDiscord, faTwitter, faYoutube,
+    faFacebookSquare, faDiscord, faTwitter, faYoutube
 } from '@fortawesome/free-brands-svg-icons';
-library.add(faFacebookSquare,faDiscord,faTwitter,faYoutube);
+library.add(faFacebookSquare,faDiscord,faTwitter,faYoutube,faDownload,faUser,faUsers,faSignInAlt,faSignOutAlt);
 dom.watch();
 
 $(document).ready(function(){
-    
+    // TOOLTIPS
+    $('[data-toggle="tooltip"]').tooltip()
 })
