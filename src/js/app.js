51f7a2c53183753c8fc74fdae19a65bc4621f5b3
Pythorogus <pierre.gomba@gmail.com>
2020-05-03 16:23:40 +0200

    aides de jeu + theme

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,13 @@ import 'popper.js';
 
 import { library,dom } from '@fortawesome/fontawesome-svg-core';
 import {
-    faDownload, faUser, faUsers, faSignInAlt, faSignOutAlt
+    faDownload, faUser, faUsers, faSignInAlt, faSignOutAlt, faHome
 } from '@fortawesome/free-solid-svg-icons';
 // import '@fortawesome/free-regular-svg-icons'
 import {
     faFacebookSquare, faDiscord, faTwitter, faYoutube
 } from '@fortawesome/free-brands-svg-icons';
-library.add(faFacebookSquare,faDiscord,faTwitter,faYoutube,faDownload,faUser,faUsers,faSignInAlt,faSignOutAlt);
+library.add(faFacebookSquare,faDiscord,faTwitter,faYoutube,faDownload,faUser,faUsers,faSignInAlt,faSignOutAlt,faHome);
 dom.watch();
 
 $(document).ready(function(){
